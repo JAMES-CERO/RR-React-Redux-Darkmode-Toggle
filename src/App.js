0d9d58af955b6a0ce5870ce56bbaeddc53750655
James Aules <james_aules@icloud.com>
2023-03-06 20:05:01 -0500

    adding toggle switch

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import './App.css';
 import Nav from './components/Nav'
 import ContentWrapper from './components/ContentWrapper'
 import Footer from './components/Footer'
+import { useSelector, useDispatch } from 'react-redux';
+import {toggleDark, toggleLight} from './components/modeSlice'
 
 function App() {
   
+  const mode = useSelector((state) => state.mode)
+  const dispatch = useDispatch()
+
+  const toggleSwitch = () => {
+    mode.darkmode 
+    ? dispatch(toggleLight())
+    : dispatch(toggleDark())
+  }
+
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
-      <Nav />
+    <div style={{ backgroundColor: mode.color1, color: 'black' }} className="App">
+      <Nav  />
+      <button onClick={toggleSwitch}>
+        toggle Me!
+      </button>
       <ContentWrapper />
       <Footer />
     </div>
