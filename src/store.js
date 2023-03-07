94f33fc6c875b5d0fcd02718f52c9675f022be6f
James Aules <james_aules@icloud.com>
2023-03-06 19:35:51 -0500

    creating store

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import modeReducer from "./components/modeSlice";
+
+const store = configureStore({
+    reducer: {
+        mode: modeReducer
+    }
+})
+ export default store
